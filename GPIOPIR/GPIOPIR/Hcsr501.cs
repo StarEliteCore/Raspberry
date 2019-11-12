4209febfe32333e6901f2523c9120518c5a05360
So魚丸丷 <dygood@outlook.com>
2019-11-12 12:57:40 +0800

    PIR人体检测DEMO

---
+++ GPIOPIR/GPIOPIR/Hcsr501.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Device.Gpio;
+
+namespace GPIOPIR
+{
+    class Hcsr501 : IDisposable
+    {
+        private GpioController _controller;
+        private readonly int _outPin;
+        public event PinChangeEventHandler pinChangeEvent;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="pin">OUT Pin</param>
+        public Hcsr501(int outPin, PinNumberingScheme pinNumberingScheme = PinNumberingScheme.Logical)
+        {
+            _outPin = outPin;
+
+            _controller = new GpioController(pinNumberingScheme);
+            _controller.OpenPin(outPin, PinMode.Input);
+            if (pinChangeEvent != null)
+                _controller.RegisterCallbackForPinValueChangedEvent(_outPin, PinEventTypes.Falling, pinChangeEvent);
+        }
+
+        // 为指定引脚的值改变时注册回调（即上文中提到的 GPIO 中断）
+        // PinEventTypes 是值改变的类型，包括上升沿（Rising，0->1）和下降沿（Falling，1->0），注意当设置为 None 时不会触发
+        // PinChangeEventHandler 为回调事件
+
+        /// <summary>
+        /// 是否检测到人体
+        /// </summary>
+        public bool IsMotionDetected => _controller.Read(_outPin) == PinValue.High;
+
+        /// <summary>
+        /// Cleanup
+        /// </summary>
+        public void Dispose()
+        {
+            _controller?.Dispose();
+            _controller = null;
+        }
+    }
+}
---
+++ GPIOPIR/GPIOPIR/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Device.Gpio;
+
+namespace GPIOPIR
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // HC-SR501 OUT Pin
+            int hcsr501Pin = 17;
+            // 初始化 PIR 传感器
+            // LED Pin
+            int ledPin = 27;
+            Blink blink = new Blink(ledPin);
+            using Hcsr501 sensor = new Hcsr501(hcsr501Pin, PinNumberingScheme.Logical);
+            sensor.pinChangeEvent += (object sender, PinValueChangedEventArgs pinValueChangedEventArgs) =>
+            {
+                blink.Flicker(pinValueChangedEventArgs.ChangeType == PinEventTypes.Rising);
+            };
+        }
+    }
+}
